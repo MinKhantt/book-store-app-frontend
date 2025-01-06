c75287c02589285e4a62f811c9cd46437c5c40db
Min Khant <djidhdh2558@gmail.com>
2025-01-06 23:32:40 +0630

    UserDashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { Outlet } from "react-router-dom";
-
+import Navbar from "./components/Navbar";
 import "./App.css";
 import Footer from "./components/Footer";
 import { AuthProvider } from "./context/AuthContext";
-import Navbar from "./components/navbar";
 
 function App() {
   return (
